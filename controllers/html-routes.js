aec6d448d0e9bc53a5c49102bb5e91e8f3d3c4ed
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-02-27 19:32:16 -0800

    Fix server initial configuraton and create database configuration

--- controllers/html-routes.js
+++
@@ -1,8 +0,0 @@
-var db = require("../models");
-var express = require("express");
-var router = express.Router();
-
-
-Router.get("/", function(req, res) {
-    res.send("Hello");
-});
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 //var session = require("express-session");
-// Requiring passport as we've configured it
 //var passport = require("./config/passport");
-const bodyParser = require("body-parser");
-const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -14,7 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// We need to use sessions to keep track of our user's login status
 //app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 //app.use(passport.initialize());
 //app.use(passport.session());
@@ -22,18 +18,16 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-  }
-
+}
 
 var apiRoutes = require("./controllers/api-routes");
 app.use(apiRoutes);
 
-var htmlRoutes = require("./controllers/html-routes");
-app.use(htmlRoutes);
+// var htmlRoutes = require("./controllers/html-routes");
+// app.use(htmlRoutes);
 
-
-db.sequelize.sync().then(function() {
-    app.listen(PORT, function() {
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
         console.log("App listening on PORT " + PORT);
     });
 });
