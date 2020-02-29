654dfb947c9e9a3ef026bbcde891e9fddac4f4c1
Shipra <drshipradyundi@gmail.com>
2020-02-29 11:00:55 -0800

    added some changes to productcard

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
-import Homepage from "./pages/Homepage";
-import Bookmarks from "./pages/Bookmarks";
+import Homepage from "./pages/homepage";
+import Bookmarks from "./pages/bookmarks";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
