cfedfa387a26e629ca83538550295a0805e4c69c
Shipra <drshipradyundi@gmail.com>
2020-03-01 16:47:47 -0800

    added few changes in productCard

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Productcard from "../components/Productcard";
-import { Input, FormBtn } from "../components/SearchBar";
-import API from "../utils/API"
+import {Input, Frmbtn} from "../components/SearchBar";
 
 
 class Homepage extends Component {
@@ -11,56 +10,46 @@ class Homepage extends Component {
     PageType: "homepage"
   };
 
-  handleFormSubmit = event => {
-
-    event.preventDefault();
-    console.log("form button clicked")
-
-    API.getProducts(this.state.giftSearch)
-      .then(res => this.setState({ products: res.data.hits }))
-      .catch(err => console.log(err));
+  handleInputChange = event => {
+      
+    const { name, value } = event.target;
+    this.setState({
+      [name] : value
+    });
   };
-  // axios.get("https://openapi.etsy.com/v2/listings/active?keywords:" + this.state.giftSearch + "&includes=Images&api_key=dggfhwkwf5yl2hsyp2mhwn38")
-  //   .then(res => {
-  //     console.log(res.data.results)
-  //     this.setState({ products: res.data.results })
-  //   }
-  //   ).catch(err => console.log(err));
+  handleFormSubmit = event => {
+    event.preventDefault();
+  
+    axios.get("" + this.state.giftSearch)
+    .then(res =>
+      //console.log(res.data.items)
+      this.setState({products: res.data.items})
+    ).catch(err => console.log(err));
+    
+  }; 
 
-handleInputChange = event => {
-
-  const { name, value } = event.target;
-  this.setState({
-    [name]: value
-  });
-};
-
-render() {
-  return (
-    <div>
-      <form>
-        <Input
+  render() {
+    return (
+      <div>
+        <SearchBar
           name="giftSearch"
           value={this.state.giftSearch}
           onChange={this.handleInputChange}
-          placeholder="Search For a Gift" />
-        <FormBtn
-          disabled={!(this.state.giftSearch)}
+          placeholder="Search For a Gift"
           onClick={this.handleFormSubmit}
-        > Search
-          </FormBtn>
-      </form>
-      {this.state.products.map(product => {
-        return (
-          <Productcard
-
-          />)
-      })}
-    </div>
-  )
+          type="success"
+          className="input-lg"
+        />
+        <Productcard
+          id="123"
+          image="https://i.etsystatic.com/5839963/d/il/ede62b/402374196/il_170x135.402374196_imoh.jpg?version=0"
+          title="Gears, Steampunk Grouping Of 12 Tiny & Small  Wood Laser Cut Gears - Size T  1&#39; or less, Metal Color - Copper, Brass, Bronze"
+          url="https://www.etsy.com/listing/265222330/gears-steampunk-grouping-of-12-tiny?utm_source=giftfinder&utm_medium=api&utm_campaign=api"
+          price="7.50"
+        />
+      </div>
+    )
+  }
 }
-}
-
-
 
 export default Homepage;
