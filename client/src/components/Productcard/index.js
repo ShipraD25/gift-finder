0c9691b0500d44d0529053c356571e33c6adace0
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-03-08 18:08:15 -0700

    style bookmark and trash icons

--- client/src/components/Productcard/index.js
+++ client/src/components/Productcard/index.js
@@ -5,28 +5,28 @@ import "./style.css";
 function showIcon(props) {
   console.log(props)
   if (props.page_type === "homepage") {
-      return <i className="fas fa-bookmark" onClick={() => props.handleBookmark(props.id)}></i>
+    return <i className="far fa-bookmark fa-lg circle-icon" onClick={() => props.handleBookmark(props.id)}></i>
   } else {
-      return <i className="fa fa-trash" aria-hidden="true" onClick={() => props.handleDelete(props.id)}></i>
+    return <i className="fa fa-trash fa-lg circle-icon" aria-hidden="true" onClick={() => props.handleDelete(props.id)}></i>
   }
 }
 
 function Productcard(props) {
   return (
     <div className="card">
-      {showIcon(props)}
       {/* <i className="fas fa-bookmark" onClick={() => props.handleBookmark(props.id)}></i> */}
       <div className="img-container">
+        {showIcon(props)}
         <img src={props.image} alt={props.id}
           onClick={() => props.handleBtnClick(props.id)} />
-          </div>
-          <div className="info">
-        <p className="info-title">
-        <span><a href={props.url} >{props.title}...</a></span>
-        </p>
-        <span className="price">{props.price}</span>
       </div>
-        </div>
+      <div className="info">
+        <p className="info-title">
+          <span><a href={props.url} >{props.title}...</a></span>
+        </p>
+        <span className="price">${props.price}</span>
+      </div>
+    </div>
   );
 }
 
